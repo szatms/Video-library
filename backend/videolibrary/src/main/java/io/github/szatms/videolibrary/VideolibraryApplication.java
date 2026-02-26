5276bc5f9d1dd409b39ec58cf377a763b2dd9c95
tomi <szabo.tamas.20040922@gmail.com>
2026-02-26 14:16:00 +0100

    Initial commit.

---
+++ backend/videolibrary/src/main/java/io/github/szatms/videolibrary/VideolibraryApplication.java
@@ -0,0 +1,13 @@
+package io.github.szatms.videolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideolibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideolibraryApplication.class, args);
+	}
+
+}
---
+++ backend/videolibrary/src/test/java/io/github/szatms/videolibrary/VideolibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.szatms.videolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideolibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
